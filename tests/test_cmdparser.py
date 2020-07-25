7b56171585def7d70059944236eadad094fae8e2
Viral Lalakia <viral.lalakia@gmail.com>
2020-07-26 03:21:19 +0530

    Feature(#7): fix lint issue

--- tests/test_cmdparser.py
+++ tests/test_cmdparser.py
@@ -8,9 +8,7 @@ from viralpycmdparser.result.cmdparserresult import CmdParserResult
     "ip, op",
     [
         pytest.param("cmd", CmdParserResult("cmd"), id="Cmd without args"),
-        pytest.param(
-            "cmd arg1", CmdParserResult("cmd", ["arg1"]), id="Cmd with 1 arg"
-        ),
+        pytest.param("cmd arg1", CmdParserResult("cmd", ["arg1"]), id="Cmd with 1 arg"),
     ],
 )
 def test_hello_world(ip: str, op: CmdParserResult) -> None:
