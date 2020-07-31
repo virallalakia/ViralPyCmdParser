31feddacbbf5f26e0a2c10b6a82401cfec980321
Viral Lalakia <viral.lalakia@gmail.com>
2020-07-31 21:32:39 +0530

    Feature(#7): add support for custom delimiter

--- src/viralpycmdparser/cmdparser.py
+++ src/viralpycmdparser/cmdparser.py
@@ -1,13 +1,16 @@
+import re
+
 from viralpycmdparser.result.cmdparserresult import CmdParserResult
 
 
 class CmdParser:
-    def __init__(self):
-        self.delim = " "
+    def __init__(self, delim=" "):
+        self.delim = delim
 
     def parse(self, cmd_str: str) -> CmdParserResult:
         result = CmdParserResult()
         if cmd_str:
+            cmd_str = re.sub(self.delim + "{2,}", self.delim, cmd_str)
             t = cmd_str.strip(self.delim).split(self.delim)
             result.cmd = t[0]
             if len(t) > 1:
--- tests/test_cmdparser.py
+++ tests/test_cmdparser.py
@@ -5,14 +5,47 @@ from viralpycmdparser.result.cmdparserresult import CmdParserResult
 
 
 @pytest.mark.parametrize(
-    "ip, op",
+    "delim, ip, op",
     [
-        pytest.param("cmd", CmdParserResult("cmd"), id="Cmd without args"),
-        pytest.param("cmd arg1", CmdParserResult("cmd", ["arg1"]), id="Cmd with 1 arg"),
+        pytest.param(None, "cmd", CmdParserResult("cmd"), id="Cmd without args"),
+        pytest.param(
+            None, "cmd arg1", CmdParserResult("cmd", ["arg1"]), id="Cmd with 1 arg"
+        ),
+        pytest.param(
+            None,
+            "cmd arg1 arg2 arg3",
+            CmdParserResult("cmd", ["arg1", "arg2", "arg3"]),
+            id="Cmd with 3 args",
+        ),
+        pytest.param(
+            None,
+            "  cmd   arg1   arg2    arg3    ",
+            CmdParserResult("cmd", ["arg1", "arg2", "arg3"]),
+            id="Cmd with 3 args and extra spaces",
+        ),
+        pytest.param(
+            ",",
+            "cmd",
+            CmdParserResult("cmd"),
+            id="Cmd without args but with custom delimiter",
+        ),
+        pytest.param(
+            ",",
+            "cmd,arg1",
+            CmdParserResult("cmd", ["arg1"]),
+            id="Cmd with 1 arg and custom delimiter",
+        ),
+        pytest.param(
+            "__",
+            "cmd__arg1 with space__arg2",
+            CmdParserResult("cmd", ["arg1 with space", "arg2"]),
+            id="Cmd with 2 args, with space and custom delimiter",
+        ),
     ],
 )
-def test_hello_world(ip: str, op: CmdParserResult) -> None:
-    result = CmdParser().parse(ip)
+def test_hello_world(delim: str, ip: str, op: CmdParserResult) -> None:
+    cmdparser = CmdParser(delim) if delim else CmdParser()
+    result = cmdparser.parse(ip)
     assert result.cmd == op.cmd
     for i in range(len(result.args)):
         assert result.args[i] == op.args[i]
